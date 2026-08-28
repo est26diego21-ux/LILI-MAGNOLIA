bbe68766d64aa9fede35a50e6b7b7be6ce86b6cf
est26diego21-ux <est26diego21@gmail.com>
2026-08-28 08:50:10 -0600

    Adaptacion del titulo de la pagina

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -27,7 +27,7 @@ export function CartProvider({ children }){
         localStorage.setItem("carrito", JSON.stringify(carrito));
     }, [carrito]);
 
-    useDocumentTitle(carrito.length === 0 ? "RETO-SHOP" : `RETO-SHOP (${carrito.length})`);
+    useDocumentTitle(carrito.length === 0 ? "LILI & MAGNOLIA" : `LILI & MAGNOLIA(${carrito.length})`);
     
     const valor = useMemo(() => ({
         carrito,
